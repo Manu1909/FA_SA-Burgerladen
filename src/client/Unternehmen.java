de2cc11d5310da09005082864adbf468e1182195
Adrian Kochsiek <adrian.kochsiek@hpe.com>
2015-11-25 21:32:12 +0100

    Controller hinzugefügt

--- src/client/Unternehmen.java
+++ src/client/Unternehmen.java
@@ -1,6 +1,7 @@
 package client;
 
 import backend.Client;
+import server.Lieferant;
 
 public class Unternehmen {
 	
@@ -11,16 +12,32 @@ public class Unternehmen {
 	private int kundenzufriedenheit;
 	private String name;
 	private Client client;
+	private Standort standort;
+	private Lieferant fleischlieferant;
+	private Lieferant brotlieferant;
+	private Lieferant salatlieferant;
+	private Lieferant sossenlieferant;
+	private Kredit kredit;
 
 	public Unternehmen(String name, Client client) {
 		this.name = name;
 		this.client = client;
 	}
 	
+	public void setzeStandort(Standort standort){
+		this.standort = standort;
+	}
+	
+	public void setzeLieferanten(Lieferant fleischlieferant, Lieferant brotlieferant, Lieferant salatlieferant, Lieferant sossenlieferant){
+		this.fleischlieferant = fleischlieferant;
+		this.brotlieferant = brotlieferant;
+		this.salatlieferant = salatlieferant;
+		this.sossenlieferant = sossenlieferant;
+	}
+	
 	public void berechneKapital(){
 		
 	}
-	
 	
 
 }
