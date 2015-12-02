59f92c96edca4c7d0893ba18c3c353fc9560e2d5
Lucas <Lucas@141.72.138.5>
2015-12-02 15:18:35 +0100

    change to newplayer.java

--- src/frontend/NewPlayer.java
+++ src/frontend/NewPlayer.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class NewPlayer extends JFrame implements ActionListener{
@@ -15,6 +17,13 @@ public class NewPlayer extends JFrame implements ActionListener{
 	private JButton next = new JButton("N‰chster Schritt");
 	private JPanel panelTop = new JPanel();
 	private JPanel panelBody = new JPanel();
+	
+	private int n;
+	private int p;
+	//Unternehmensinfos erstes Fenster
+	private String location;
+	private String cmpname;
+
 	
 	public static void main(String[] args) 
 	{
@@ -28,14 +37,20 @@ public class NewPlayer extends JFrame implements ActionListener{
 	
 	public void buildWindow()
 	{
+		JLabel introduction = new JLabel("<html><body><h1 "
+				+ "color=red>Burgerplanspiel</h1>"
+				+ "<br>Textzeile2</body></html>");
+		panelTop.add(introduction);
 		panelTop.add(go);
 		
-		spielerAufbau.add(panelTop, BorderLayout.NORTH);
-		spielerAufbau.add(panelBody, BorderLayout.CENTER);
+		go.addActionListener(this);
+		spielerAufbau.add(panelTop, BorderLayout.CENTER);
+		spielerAufbau.add(panelBody, BorderLayout.SOUTH);
 		spielerAufbau.setBounds(1,1,400,300);
 		spielerAufbau.setExtendedState(MAXIMIZED_BOTH);
 		spielerAufbau.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		spielerAufbau.setVisible(true);
+		
 		
 	}
 
@@ -45,10 +60,33 @@ public class NewPlayer extends JFrame implements ActionListener{
 		Object s = e.getSource();// TODO Auto-generated method stub
 		if(s == go)
 		{
-			go.setVisible(false);
-//			panelTop.remove(go);
-//			panelTop.revalidate();
-//			panelTop.repaint();
+			while( p != 1)
+			{
+				cmpname = JOptionPane.showInputDialog("Bitte geben Sie ihrem Laden einen Namen");
+				Object[] yesno = {"Nein", "Ja"};
+				p = JOptionPane.showOptionDialog(null,
+		            "Sind Sie sicher, dass ihr Laden '"+ cmpname +"' heiﬂen soll?",
+		            "Best‰tigung",
+		            JOptionPane.YES_NO_OPTION,
+		            JOptionPane.DEFAULT_OPTION,
+		            null,
+		            yesno,
+		            yesno[1]);
+			}
+	
+			
+			if(p == 1){
+			Object[] options = {"Planken","Jungbusch", "Schloss" };       
+			n = JOptionPane.showOptionDialog(null,
+			           "W‰hlen Sie den Standort ihres Ladens",
+			           "Standortwahl",
+			           JOptionPane.YES_NO_OPTION,
+			           JOptionPane.DEFAULT_OPTION,
+			           null,
+			           options,
+			           options[1]);
+			}
+		
 		}
 	}
 }
